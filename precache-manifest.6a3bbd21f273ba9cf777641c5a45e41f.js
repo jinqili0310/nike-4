7dc0d4b73931125ff141f9e399aa5c30e05d2324
Jinqi Li <jxl2047@miami.edu>
2020-12-01 03:31:34 -0500

    Updates

---
+++ precache-manifest.6a3bbd21f273ba9cf777641c5a45e41f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bbb4076e4f76366403a5ea7fe90124",
+    "url": "/nike-4/index.html"
+  },
+  {
+    "revision": "5b39b2a7b1d18780582f",
+    "url": "/nike-4/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "ced6d1e8964b3207cbaf",
+    "url": "/nike-4/static/css/main.a450ef72.chunk.css"
+  },
+  {
+    "revision": "5b39b2a7b1d18780582f",
+    "url": "/nike-4/static/js/2.e0dbbd01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nike-4/static/js/2.e0dbbd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced6d1e8964b3207cbaf",
+    "url": "/nike-4/static/js/main.ef51bb28.chunk.js"
+  },
+  {
+    "revision": "47a032fa1ebc2a7361eb",
+    "url": "/nike-4/static/js/runtime-main.023fe06b.js"
+  },
+  {
+    "revision": "4552cc5febe5118eed88e01365980e42",
+    "url": "/nike-4/static/media/Futura Condensed Extra Bold Oblique.4552cc5f.otf"
+  },
+  {
+    "revision": "578770257fc22c53a451915ca0625eb5",
+    "url": "/nike-4/static/media/Futura Condensed Extra Bold.57877025.otf"
+  },
+  {
+    "revision": "aafd77248d9c8b52795e14aec340ec21",
+    "url": "/nike-4/static/media/Trade Gothic LT Bold Oblique.aafd7724.ttf"
+  },
+  {
+    "revision": "cb7d0b0968943ceca839712e38df14c1",
+    "url": "/nike-4/static/media/Trade Gothic LT Bold.cb7d0b09.ttf"
+  },
+  {
+    "revision": "05748951114e8f8876ccfcaca8c8e7f2",
+    "url": "/nike-4/static/media/Trade Gothic LT Light.05748951.ttf"
+  },
+  {
+    "revision": "e53b0db15e6bb41eb09e467de78e2fff",
+    "url": "/nike-4/static/media/Trade Gothic LT Oblique.e53b0db1.ttf"
+  },
+  {
+    "revision": "0631e2714248f8295ff4d71b602a486d",
+    "url": "/nike-4/static/media/Trade Gothic LT.0631e271.ttf"
+  },
+  {
+    "revision": "575aa4e1b796d7068d84d987b251ead6",
+    "url": "/nike-4/static/media/air-max-270-react-se-womens-shoe-npb2CK.575aa4e1.jpg"
+  },
+  {
+    "revision": "4bae5f695a65d46667ba1a49749707a4",
+    "url": "/nike-4/static/media/air-max-90-womens-shoe-S6hW4R.4bae5f69.jpg"
+  },
+  {
+    "revision": "aeacebc6a238082d488094bc9efea32b",
+    "url": "/nike-4/static/media/image1.aeacebc6.jpg"
+  },
+  {
+    "revision": "73de74d434cd66d1c442e46be3508c67",
+    "url": "/nike-4/static/media/image2.73de74d4.jpg"
+  },
+  {
+    "revision": "9ec013b88df1001f8f079f4be9fff118",
+    "url": "/nike-4/static/media/image3.9ec013b8.jpg"
+  },
+  {
+    "revision": "3a25fe98d7535d26d3d1747c0ff88e10",
+    "url": "/nike-4/static/media/joyride-dual-run-mens-running-shoe-8BGXtk.3a25fe98.jpg"
+  },
+  {
+    "revision": "bb54b5b91cdd1a47d6d927b576e0a56c",
+    "url": "/nike-4/static/media/nike-just-do-it-1.bb54b5b9.jpg"
+  },
+  {
+    "revision": "a6db3007acbd9a6f6239d2fffe2a22de",
+    "url": "/nike-4/static/media/nike-just-do-it-2.a6db3007.jpg"
+  },
+  {
+    "revision": "e5c2e894f6ecc2fc9bb2be0989882208",
+    "url": "/nike-4/static/media/nike-just-do-it-3.e5c2e894.jpg"
+  },
+  {
+    "revision": "271f9022e735551dcb491df8644dcfbb",
+    "url": "/nike-4/static/media/nike-just-do-it-4.271f9022.jpg"
+  },
+  {
+    "revision": "c5c55786bc169e6fb30bc42ab908ea59",
+    "url": "/nike-4/static/media/nike-just-do-it-5.c5c55786.jpg"
+  },
+  {
+    "revision": "abf256a1b6c12b8db6b3fdd82bc8445e",
+    "url": "/nike-4/static/media/nike-just-do-it.abf256a1.jpg"
+  },
+  {
+    "revision": "076b6d8ab2dac32c10f6b45e6e3a19e9",
+    "url": "/nike-4/static/media/nike-logo.076b6d8a.png"
+  }
+]);
